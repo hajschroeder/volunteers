913e8b4bca89773de4277c34b0832e266d860ed6
Hans Schroeder <hajschroeder@gmail.com>
2022-05-19 15:17:02 -0700

    uncomment out tests for .all to account for #save. #save and .all methods written. Green

--- lib/project.rb
+++ lib/project.rb
@@ -15,5 +15,21 @@ class Project
     self.title() == project_to_compare.title()
   end
 
+  def self.all
+    returned_projects = DB.exec("SELECT * FROM projects;")
+    projects = []
+    returned_projects.each() do |project|
+      title = project.fetch("title")
+      id = project.fetch("id")
+      projects.push(Project.new({:title => title, :id => id}))
+    end
+    projects
+  end
+
+  def save
+    result =  DB.exec("INSERT INTO projects (title) VALUES ('#{title}') RETURNING id;")
+    @id = result.first().fetch("id").to_i
+  end
+
 
 end
