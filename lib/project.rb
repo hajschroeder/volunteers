cfcb2bb5270180b4a2cc344d3f16f6b1c43a0a11
Hans Schroeder <hajschroeder@gmail.com>
2022-05-19 15:10:22 -0700

    commented out #title method and test. Uncommented #== test and wrote method. Green

--- lib/project.rb
+++ lib/project.rb
@@ -1,10 +1,19 @@
 class Project
   attr_reader :id
-  attr_accessor :name
+  attr_accessor :title
 
   def initialize(attributes)
-    @name = attributes.fetch(:name)
+    @title = attributes.fetch(:title)
     @id = attributes.fetch(:id)
   end
+
+  # def title 
+  #   project_title = DB.exec("SELECT title FROM projects;")
+  # end
+
+  def ==(project_to_compare)
+    self.title() == project_to_compare.title()
+  end
+
 
 end
--- spec/project_spec.rb
+++ spec/project_spec.rb
@@ -1,12 +1,12 @@
 require "spec_helper"
 
 describe Project do
-  describe '#title' do
-    it 'returns the project title' do
-      project = Project.new({:title => 'Teaching Kids to Code', :id => nil})
-      expect(project.title).to eq 'Teaching Kids to Code'
-    end
-  end
+  # describe '#title' do
+  #   it 'returns the project title' do
+  #     project = Project.new({:title => 'Teaching Kids to Code', :id => nil})
+  #     expect(project.title).to eq 'Teaching Kids to Code'
+  #   end
+  # end
 
   # context '#id' do
   #   it 'returns the id of the project before saving project' do
@@ -21,13 +21,13 @@ describe Project do
   #   end
   # end
 
-  # describe '#==' do
-  #   it 'is the same project if two projects have the same title' do
-  #     project1 = Project.new({:title => 'Teaching Kids to Code', :id => nil})
-  #     project2 = Project.new({:title => 'Teaching Kids to Code', :id => nil})
-  #     expect(project1 == project2).to eq true
-  #   end
-  # end
+  describe '#==' do
+    it 'is the same project if two projects have the same title' do
+      project1 = Project.new({:title => 'Teaching Kids to Code', :id => nil})
+      project2 = Project.new({:title => 'Teaching Kids to Code', :id => nil})
+      expect(project1 == project2).to eq true
+    end
+  end
 
   # context '.all' do
   #   it 'is empty to start' do
