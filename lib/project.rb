ba4b66fd05a8cf9c396533199a22130f80aa9e5f
Hans Schroeder <hajschroeder@gmail.com>
2022-05-20 10:48:04 -0700

    include code for .find method. Green

--- lib/project.rb
+++ lib/project.rb
@@ -31,5 +31,16 @@ class Project
     @id = result.first().fetch("id").to_i
   end
 
+  def self.find(id)
+    project = DB.exec("SELECT * FROM projects WHERE id = #{id};").first
+    if project 
+      title = project.fetch("title")
+      id = project.fetch("id").to_i
+      Project.new({:title => title, :id => id})
+    else
+      nil
+    end
+  end
+
 
 end
