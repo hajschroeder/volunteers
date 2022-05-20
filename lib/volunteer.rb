fad8a350043c0ee8cea4b692151dd2dc63628b29
Hans Schroeder <hajschroeder@gmail.com>
2022-05-20 11:53:29 -0700

    recommented tests in project_spec. Uncommented #== test in volunteer_spec and wrote method. Green

--- lib/volunteer.rb
+++ lib/volunteer.rb
@@ -8,4 +8,13 @@ class Volunteer
     @id = attributes.fetch(:id)
   end
 
+  def ==(volunteer_to_compare)
+    if volunteer_to_compare != nil 
+      (self.name() == volunteer_to_compare.name()) && (self.project_id() == volunteer_to_compare.project_id())
+    else
+      false
+    end
+  end
+  
+
 end
--- spec/volunteer_spec.rb
+++ spec/volunteer_spec.rb
@@ -1,6 +1,6 @@
-# require "spec_helper"
+require "spec_helper"
 
-# describe Volunteer do
+describe Volunteer do
 #   describe '#name' do
 #     it 'returns the name of the volunteer' do
 #       test_volunteer = Volunteer.new({:name => 'Jane', :project_id => 1, :id => nil})
@@ -15,13 +15,13 @@
 #     end
 #   end
 
-#   describe '#==' do
-#     it 'checks for equality based on the name of a volunteer' do
-#       volunteer1 = Volunteer.new({:name => 'Jane', :project_id => 1, :id => nil})
-#       volunteer2 = Volunteer.new({:name => 'Jane', :project_id => 1, :id => nil})
-#       expect(volunteer1 == volunteer2).to eq true
-#     end
-#   end
+  describe '#==' do
+    it 'checks for equality based on the name of a volunteer' do
+      volunteer1 = Volunteer.new({:name => 'Jane', :project_id => 1, :id => nil})
+      volunteer2 = Volunteer.new({:name => 'Jane', :project_id => 1, :id => nil})
+      expect(volunteer1 == volunteer2).to eq true
+    end
+  end
 
 #   context '.all' do
 #     it 'is empty to start' do
@@ -54,4 +54,4 @@
 #       expect(Volunteer.find(volunteer1.id)).to eq volunteer1
 #     end
 #   end
-# end
+end
