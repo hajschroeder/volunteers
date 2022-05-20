1f728b60295396db69a36c7df024b35c20db35c0
Hans Schroeder <hajschroeder@gmail.com>
2022-05-20 13:21:05 -0700

    uncomment #update method for project_spec. Wrote method. Green

--- lib/project.rb
+++ lib/project.rb
@@ -45,5 +45,12 @@ class Project
     end
   end
 
+  def update(attributes)
+    if (attributes.has_key?(:title)) && (attributes.fetch(:title) != nil)
+      @title = attributes.fetch(:title)
+      DB.exec("UPDATE projects SET title = '#{@title}' WHERE id = #{@id};")
+    end
+  end
+  
 
 end
