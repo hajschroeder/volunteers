c490f94d55be492378e907436d55172d25d04036
Hans Schroeder <hajschroeder@gmail.com>
2022-05-21 11:10:40 -0700

    begin writing routes for projects

---
+++ app.rb
@@ -0,0 +1,33 @@
+require('sinatra')
+require('sinatra/reloader')
+require('./lib/project')
+require('./lib/volunteer')
+require('pry')
+require('pg')
+also_reload('lib/**/*.rb')
+
+DB = PG.connect({:dbname => "volunteer_tracker"})
+
+get('/') do 
+  @projects = Project.all
+  erb(:projects)
+end
+
+get('/projects') do 
+  @projects = Project.all
+  erb(:projects)
+end
+
+post('/projects') do 
+  title = params[:project_title]
+  project = Project.new(:title => title, :id => nil)
+  project.save()
+  @projects = Project.all
+  erb(:projects)
+end
+
+get('projects/new') do 
+  erb(:new_project)
+end
+
+
